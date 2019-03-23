62fa36c38732669c12b120c7463a2f5e2d9f473b
vikas chauhan <chvikaschauhan@gmail.com>
2019-03-23 21:49:01 +0530

    issue fixed

--- controller/status.ts
+++ controller/status.ts
@@ -6,11 +6,6 @@ export let send = (req: Request, res: Response) => {
    
      var msg=req.query.msg;
      // connect to rabbitmq
-    
-
-
-
-
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'hello';
@@ -18,36 +13,33 @@ amqp.connect('amqp://localhost', function(err, conn) {
     ch.assertQueue(q, {durable: false});
     // Note: on Node 6 Buffer.from(msg) should be used
     ch.sendToQueue(q,  Buffer.from(msg));
-    console.log(" [x] Sent 'Hello World!'");
+    console.log(" [x] sent ",msg);
   });
- // setTimeout(function() { conn.close(); process.exit(0) }, 500);
+  res.send("message send"+msg);
+ //setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
-
-res.send("message send"+msg);
 
 }
 
 export let receiver = (req: Request, res: Response) => {
-  //  res.send("how's it going?")
-
-var msg2
-    // add receiver
-
-    
-
+       // add receiver
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'hello';
+    
 
     ch.assertQueue(q, {durable: false});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
-      msg2= msg.content.toString();
-      res.send("message="+msg2);
+    var msg2 =  msg.content.toString();
+ 
+     // res.send("message=" +  msg.content.toString());
       console.log(" [x] Received %s", msg.content.toString());
+      res.send(msg2);
       
     }, {noAck: true});
   });
+  
 });
 
 }
